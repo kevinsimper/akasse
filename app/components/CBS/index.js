52ab49702b38abff8e411430743f934dd38272db
Mathias Lund <mathias@graduateland.com>
2016-03-06 23:06:03 +0100

    added cbs folder

---
+++ app/components/CBS/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class CBS extends Component {
+  render() {
+    return (
+      <p>CBS</p>
+    )
+  }
+}
--- app/routes.js
+++ app/routes.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { Router, Route, IndexRoute } from 'react-router'
 import Main from './components/Main'
 import Companies from './components/Companies'
+import CBS from './components/CBS'
 
 export default (
   <Route path='/' component={Main}>
     <IndexRoute component={Companies}/>
+    <Route path='cbs' component={CBS}/>
   </Route>
 )
